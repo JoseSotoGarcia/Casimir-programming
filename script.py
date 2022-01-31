ac8ba3344da1b1f835f3f61bdcfde64095b70601
Jose Soto <josesotogarci@gmail.com>
2022-01-31 12:04:37 +0100

    fixed error in test.py, added test.py to script.py

--- script.py
+++ script.py
@@ -1,1 +1,5 @@
+import test
 print("Hello world")
+r = 3
+print(test.circunference_circle(r))
+print(test.area_circle(r))
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def circunference_circle(r):
     circunference = 2*np.pi*r
     return(circunference)
 
-def area_circle(r)
+def area_circle(r):
     area = np.pi*r**2
     return(area)
 
